4509d55cdc7f8fae149ae9e9df3a748c27262250
FiereEinar <nickxylanmelloria@gmail.com>
2023-12-01 17:25:29 +0800

    succesfully transfered to html

--- home.js
+++ home.js
@@ -1,96 +1,53 @@
-export const home = {
-    render: (parent) => {
-        const container = document.createElement('div');
-        container.className = 'container';
-        parent.appendChild(container);
+const img = document.querySelector('.container > img');
+const text = document.querySelector('.container > div > .aboutContainer > .domText');
 
-        const content = document.createElement('div');
-        container.appendChild(content);
+setTimeout(() => {
+    let up = true;
+    let showCareer = false;
+    let whatToShow;
 
-        const span1 = document.createElement('span');
-        span1.className = 'blur';
-        const span2 = document.createElement('span');
-        span2.className = 'blur';
-        content.appendChild(span1);
-        content.appendChild(span2);
+    const imgInterval = setInterval(() => {
+        up = !up;
+        up ? img.className = 'up' : img.className = 'down';
+    }, 1000);
 
-        const header = document.createElement('h1');
-        header.innerHTML = "Hi! I'm Nick!";
-        content.appendChild(header);
+    typeToScreen('a 1st year College Student');
 
-        const aboutContainer = document.createElement('div');
-        aboutContainer.className = 'aboutContainer';
-        content.appendChild(aboutContainer);
+    const textInterval = setInterval(() => {
+        let i = 0;
+        
+        showCareer = !showCareer;
+        showCareer 
+        ? whatToShow = 'an aspiring Full Stack Developer' 
+        : whatToShow = 'a 1st year College Student';
 
-        const about = document.createElement('h4');
-        const text = document.createElement('h4');
-        text.className = 'domText';
-        about.innerHTML = "And I'm ";
-        aboutContainer.appendChild(about);
-        aboutContainer.appendChild(text);
+        typeToScreen(whatToShow);
+    }, 7500);
 
-        const details = document.createElement('p');
-        details.innerHTML = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis accusantium dolorem, exercitationem natus illum molestias.';
-        content.appendChild(details);
-
-        const button = document.createElement('button');
-        button.innerHTML = 'View Projects >';
-        content.appendChild(button);
-
-        const img = document.createElement('img');
-        img.src = 'imgs/hero-removebg.png';
-        container.appendChild(img);
-
-        setTimeout(() => {
-            
-            let up = true;
-            let showCareer = true;
-            container.classList.add('show');
-
-            const imgInterval = setInterval(() => {
-                up = !up;
-                up ? img.className = 'up' : img.className = 'down';
-            }, 1000);
-
-            let career = 'an aspiring Full Stack Developer';
-            let college = 'a 1st year College Student';
-
-            typeToScreen(career);
-
-            const textInterval = setInterval(() => {
-                let i = 0;
-                let whatToShow = '';
-                showCareer = !showCareer;
-                showCareer ? whatToShow = career : whatToShow = college;
-
-                typeToScreen(whatToShow);
-            }, 7500);
-
-            function clearText(text) {
-                const domText = document.querySelector('.domText');
-                let word = text
-                const clearTextInterval = setInterval(() => {
-                    word = word.slice(0, -1);
-                    domText.innerHTML = word;
-                    if (word.length == 0) {
-                        clearInterval(clearTextInterval);
-                    }
-                }, 100);
+    function typeToScreen(word) {
+        let i = 0;
+        const typeInterval = setInterval(() => {
+            text.innerHTML += word[i];
+            i++;
+            if (i == word.length) {
+                setTimeout(() => {
+                    clearText(word);
+                }, 800);
+                clearInterval(typeInterval);
             }
+        }, 100);
+    }
 
-            function typeToScreen(word) {
-                let i = 0;
-                const typeInterval = setInterval(() => {
-                    text.innerHTML += word[i];
-                    i++;
-                    if (i == word.length) {
-                        setTimeout(() => {
-                            clearText(word);
-                        }, 800);
-                        clearInterval(typeInterval);
-                    }
-                }, 100);
+    function clearText(text) {
+        const domText = document.querySelector('.domText');
+
+        let word = text
+        const clearTextInterval = setInterval(() => {
+            word = word.slice(0, -1);
+            domText.innerHTML = word;
+            if (word.length == 0) {
+                clearInterval(clearTextInterval);
             }
-        }, 50)
-    },
-}
+        }, 100);
+    }
+}, 50);
