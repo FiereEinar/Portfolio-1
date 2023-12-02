ee62bcd1266aa9d5572d52a781d19bd5f426b7d9
FiereEinar <nickxylanmelloria@gmail.com>
2023-12-02 09:14:35 +0800

    added animations

--- home.js
+++ home.js
@@ -1,5 +1,30 @@
 const img = document.querySelector('.container > img');
 const text = document.querySelector('.container > div > .aboutContainer > .domText');
+const main = document.querySelector('.main > .container > div');
+const mainImg = document.querySelector('.main > .container > img');
+const nav = document.querySelector('.navBar');
+const leftCards = document.querySelectorAll('.cardContainer > .left');
+const rightCards = document.querySelectorAll('.cardContainer > .right');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+observer.observe(main);
+observer.observe(mainImg);
+observer.observe(nav);
+leftCards.forEach((card) => {
+    observer.observe(card);
+});
+rightCards.forEach((card) => {
+    observer.observe(card);
+});
 
 setTimeout(() => {
     let up = true;
@@ -8,7 +33,7 @@ setTimeout(() => {
 
     const imgInterval = setInterval(() => {
         up = !up;
-        up ? img.className = 'up' : img.className = 'down';
+        up ? img.id = 'up' : img.id = 'down';
     }, 1000);
 
     typeToScreen('a 1st year College Student');
@@ -23,31 +48,31 @@ setTimeout(() => {
 
         typeToScreen(whatToShow);
     }, 7500);
+}, 500);
 
-    function typeToScreen(word) {
-        let i = 0;
-        const typeInterval = setInterval(() => {
-            text.innerHTML += word[i];
-            i++;
-            if (i == word.length) {
-                setTimeout(() => {
-                    clearText(word);
-                }, 800);
-                clearInterval(typeInterval);
-            }
-        }, 100);
-    }
+function typeToScreen(word) {
+    let i = 0;
+    const typeInterval = setInterval(() => {
+        text.innerHTML += word[i];
+        i++;
+        if (i == word.length) {
+            setTimeout(() => {
+                clearText(word);
+            }, 800);
+            clearInterval(typeInterval);
+        }
+    }, 100);
+}
 
-    function clearText(text) {
-        const domText = document.querySelector('.domText');
+function clearText(text) {
+    const domText = document.querySelector('.domText');
 
-        let word = text
-        const clearTextInterval = setInterval(() => {
-            word = word.slice(0, -1);
-            domText.innerHTML = word;
-            if (word.length == 0) {
-                clearInterval(clearTextInterval);
-            }
-        }, 100);
-    }
-}, 50);
+    let word = text
+    const clearTextInterval = setInterval(() => {
+        word = word.slice(0, -1);
+        domText.innerHTML = word;
+        if (word.length == 0) {
+            clearInterval(clearTextInterval);
+        }
+    }, 100);
+}
