2223abb9900aca5cff198656d0d9687c60916760
nickxylanmelloria@gmail.com <nickxylanmelloria@gmail.com>
2023-12-22 15:57:46 +0800

    1.5 rework [ folder structure, animations ]

--- src/js/flex.js
+++ src/js/flex.js
@@ -1,5 +1,68 @@
 import { observer } from '../js/observer.js';
 
+const text = document.querySelector('.domText');
 const nav = document.querySelector('.navBar');
+const mainImg = document.querySelector('.main > .container > img');
+const main = document.querySelector('.main > .container > div');
+const leftCards = document.querySelectorAll('.cardContainer > .left');
+const rightCards = document.querySelectorAll('.cardContainer > .right');
 
 observer.observe(nav);
+observer.observe(main);
+observer.observe(mainImg);
+leftCards.forEach((card) => observer.observe(card));
+rightCards.forEach((card) => observer.observe(card));
+
+const navButton = document.querySelector('.navButton');
+const ul = document.querySelector('.navBar > ul');
+
+navButton.addEventListener('click', () => {
+	ul.classList.toggle('showUL');
+});
+
+setTimeout(() => {
+	let textSwitch = false;
+	let word;
+
+	typeToScreen('a 1st year College Student');
+
+	const textInterval = setInterval(() => {
+		let i = 0;
+
+		textSwitch = !textSwitch;
+		textSwitch ?
+			word = 'an aspiring Full Stack Developer' :
+			word = 'a 1st year College Student';
+
+		typeToScreen(word);
+	}, 7500);
+}, 500);
+
+function typeToScreen(word) {
+	let i = 0;
+	let textTemp = '';
+	const typeInterval = setInterval(() => {
+		textTemp += word[i];
+		text.innerHTML = textTemp + '|';
+		i++;
+		if (i == word.length) {
+			setTimeout(() => {
+				clearText(word);
+			}, 800);
+			clearInterval(typeInterval);
+		}
+	}, 100);
+}
+
+function clearText(text) {
+	const domText = document.querySelector('.domText');
+
+	let word = text
+	const clearTextInterval = setInterval(() => {
+		word = word.slice(0, -1);
+		domText.innerHTML = word + '|';
+		if (word.length == 0) {
+			clearInterval(clearTextInterval);
+		}
+	}, 100);
+}
--- src/js/home.js
+++ src/js/home.js
@@ -1,13 +1,12 @@
 import { observer } from '../js/observer.js';
 
-const img = document.querySelector('.container > img');
-const mainContainer = document.querySelector('.container');
+// const mainContainer = document.querySelector('.container');
 const text = document.querySelector('.domText');
 const main = document.querySelector('.main > .container > div');
 const mainImg = document.querySelector('.main > .container > img');
 const nav = document.querySelector('.navBar');
-const leftCards = document.querySelectorAll('.cardContainer > .left');
-const rightCards = document.querySelectorAll('.cardContainer > .right');
+// const leftCards = document.querySelectorAll('.cardContainer > .left');
+// const rightCards = document.querySelectorAll('.cardContainer > .right');
 const imgsContainer = document.querySelector('.imgsContainer');
 const details = document.querySelector('.details');
 const contact = document.querySelector('.contact');
@@ -24,14 +23,14 @@ const contact = document.querySelector('.contact');
 // });
 
 observer.observe(main);
-observer.observe(mainContainer);
+// observer.observe(mainContainer);
 observer.observe(mainImg);
 observer.observe(nav);
 observer.observe(imgsContainer);
 observer.observe(details);
 observer.observe(contact);
-leftCards.forEach((card) => observer.observe(card));
-rightCards.forEach((card) => observer.observe(card));
+// leftCards.forEach((card) => observer.observe(card));
+// rightCards.forEach((card) => observer.observe(card));
 
 
 const navButton = document.querySelector('.navButton');
@@ -43,48 +42,46 @@ navButton.addEventListener('click', () => {
 
 
 setTimeout(() => {
-    let textSwitch = false;
-    let word;
+	let textSwitch = false;
+	let word;
 
-    typeToScreen('a 1st year College Student');
+	typeToScreen('a 1st year College Student');
 
-    const textInterval = setInterval(() => {
-        let i = 0;
-        
-        textSwitch = !textSwitch;
-        textSwitch 
-        ? word = 'an aspiring Full Stack Developer' 
-        : word = 'a 1st year College Student';
+	const textInterval = setInterval(() => {
+		textSwitch = !textSwitch;
+		textSwitch ?
+			word = 'an aspiring Full Stack Developer' :
+			word = 'a 1st year College Student';
 
-        typeToScreen(word);
-    }, 7500);
+		typeToScreen(word);
+	}, 7500);
 }, 500);
 
 function typeToScreen(word) {
-    let i = 0;
-    let textTemp = '';
-    const typeInterval = setInterval(() => {
-    	textTemp += word[i];
-        text.innerHTML = textTemp + '|';
-        i++;
-        if (i == word.length) {
-            setTimeout(() => {
-                clearText(word);
-            }, 800);
-            clearInterval(typeInterval);
-        }
-    }, 100);
+	let i = 0;
+	let textTemp = '';
+	const typeInterval = setInterval(() => {
+		textTemp += word[i];
+		text.innerHTML = textTemp + '|';
+		i++;
+		if (i == word.length) {
+			setTimeout(() => {
+				clearText(word);
+			}, 800);
+			clearInterval(typeInterval);
+		}
+	}, 100);
 }
 
 function clearText(text) {
-    const domText = document.querySelector('.domText');
+	const domText = document.querySelector('.domText');
 
-    let word = text
-    const clearTextInterval = setInterval(() => {
-        word = word.slice(0, -1);
-        domText.innerHTML = word + '|';
-        if (word.length == 0) {
-            clearInterval(clearTextInterval);
-        }
-    }, 100);
+	let word = text
+	const clearTextInterval = setInterval(() => {
+		word = word.slice(0, -1);
+		domText.innerHTML = word + '|';
+		if (word.length == 0) {
+			clearInterval(clearTextInterval);
+		}
+	}, 100);
 }
