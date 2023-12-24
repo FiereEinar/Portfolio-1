2e57e5ef27d4d7d192d53df5de8ba3d75c72406b
FiereEinar <nickxylanmelloria@gmail.com>
2023-12-24 10:43:54 +0800

    blog page done

--- src/js/blog.js
+++ src/js/blog.js
@@ -3,10 +3,16 @@ import { observer } from "../js/observer.js";
 const nav = document.querySelector('.navBar');
 const pe = document.querySelector('.pe');
 const tree = document.querySelector('.tree');
+const blogIntro = document.querySelector('.blogIntro');
+const left = document.querySelectorAll('.lessonsContainer > div > .left');
+const right = document.querySelectorAll('.lessonsContainer > div > .right');
 
 observer.observe(nav);
 observer.observe(pe);
 observer.observe(tree);
+observer.observe(blogIntro);
+left.forEach(card => observer.observe(card));
+right.forEach(card => observer.observe(card));
 
 const navButton = document.querySelector('.navButton');
 const ul = document.querySelector('.navBar > ul');
